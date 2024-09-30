371670acadfc27020b6c52e0f70bcb92de677b4d
SasinduSrimal <sasindusrimal123@gmail.com>
2024-09-30 20:27:38 +0530

    updated

---
+++ Tea_Fac_Management/Form3.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Tea_Fac_Management
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.dtpDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.dtpDateOfJoin = new System.Windows.Forms.DateTimePicker();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtContactNo = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNationalId = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(371, 406);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 42;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(231, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 40;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cmbGender.Location = new System.Drawing.Point(346, 246);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(121, 24);
+            this.cmbGender.TabIndex = 39;
+            // 
+            // dtpDateOfBirth
+            // 
+            this.dtpDateOfBirth.Location = new System.Drawing.Point(346, 314);
+            this.dtpDateOfBirth.Name = "dtpDateOfBirth";
+            this.dtpDateOfBirth.Size = new System.Drawing.Size(200, 22);
+            this.dtpDateOfBirth.TabIndex = 38;
+            // 
+            // dtpDateOfJoin
+            // 
+            this.dtpDateOfJoin.Location = new System.Drawing.Point(346, 347);
+            this.dtpDateOfJoin.Name = "dtpDateOfJoin";
+            this.dtpDateOfJoin.Size = new System.Drawing.Size(200, 22);
+            this.dtpDateOfJoin.TabIndex = 37;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(346, 148);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 36;
+            // 
+            // txtContactNo
+            // 
+            this.txtContactNo.Location = new System.Drawing.Point(346, 184);
+            this.txtContactNo.Name = "txtContactNo";
+            this.txtContactNo.Size = new System.Drawing.Size(100, 22);
+            this.txtContactNo.TabIndex = 35;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(346, 280);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 22);
+            this.txtAddress.TabIndex = 34;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(346, 219);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 33;
+            // 
+            // txtNationalId
+            // 
+            this.txtNationalId.Location = new System.Drawing.Point(346, 108);
+            this.txtNationalId.Name = "txtNationalId";
+            this.txtNationalId.Size = new System.Drawing.Size(100, 22);
+            this.txtNationalId.TabIndex = 32;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(209, 352);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(74, 16);
+            this.label10.TabIndex = 31;
+            this.label10.Text = "Date of join";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(210, 320);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 16);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Date of birth";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(210, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 16);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "Gender";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(210, 286);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 16);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(210, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 16);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Worker Details";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(210, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(210, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 16);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Contact No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 16);
+            this.label2.TabIndex = 23;
+            this.label2.Text = " ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(282, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 22);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Register New Worker ";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(672, 406);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 43;
+            this.button6.Text = "Home";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.dtpDateOfBirth);
+            this.Controls.Add(this.dtpDateOfJoin);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtContactNo);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNationalId);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Registration";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.DateTimePicker dtpDateOfBirth;
+        private System.Windows.Forms.DateTimePicker dtpDateOfJoin;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtContactNo;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNationalId;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+    }
+}
---
+++ Tea_Fac_Management/Form3.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Tea_Fac_Management
+{
+    public partial class Form3 : Form
+    {
+        
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Validate all fields
+            if (string.IsNullOrWhiteSpace(txtNationalId.Text) ||
+                string.IsNullOrWhiteSpace(txtName.Text) ||
+                string.IsNullOrWhiteSpace(txtContactNo.Text) ||
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                cmbGender.SelectedIndex == -1 ||
+                string.IsNullOrWhiteSpace(txtAddress.Text) ||
+                dtpDateOfBirth.Value == DateTime.MinValue ||
+                dtpDateOfJoin.Value == DateTime.MinValue)
+            {
+                MessageBox.Show("Please fill all the information.");
+                return;
+            }
+
+            // Get the values from the form
+            string nationalId = txtNationalId.Text;
+            string name = txtName.Text;
+            string contactNo = txtContactNo.Text;
+            string email = txtEmail.Text;
+            string gender = cmbGender.SelectedItem.ToString();
+            string address = txtAddress.Text;
+            DateTime dateOfBirth = dtpDateOfBirth.Value;
+            DateTime dateOfJoin = dtpDateOfJoin.Value;
+
+            // Connection string to  SQL Server database
+            string connectionString = "Server=DESKTOP-KO8C234\\SQLEXPRESS;Database=TFMdb;Integrated Security=True;";
+
+            // SQL query to check if the national id already exists
+            string checkQuery = "SELECT COUNT(*) FROM workers WHERE national_id = @NationalId";
+
+            // SQL query to insert a new worker record
+            string insertQuery = "INSERT INTO workers (factory_id, national_id, name, contact_no, email, gender, address, date_of_birth, date_of_join) " +
+                                 "VALUES (@FactoryId, @NationalId, @Name, @ContactNo, @Email, @Gender, @Address, @DateOfBirth, @DateOfJoin)";
+
+            // Use SqlConnection and SqlCommand to check for existing national id
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (SqlCommand checkCommand = new SqlCommand(checkQuery, connection))
+                    {
+                        checkCommand.Parameters.AddWithValue("@NationalId", nationalId);
+                        int count = (int)checkCommand.ExecuteScalar();
+
+                        if (count > 0)
+                        {
+                            MessageBox.Show("This worker is already registered.");
+                            return;
+                        }
+                    }
+
+                    // Generate a unique ID for factory_id
+                    string factoryId = GenerateUniqueId(connection);
+
+                    // Use SqlCommand to insert data
+                    using (SqlCommand insertCommand = new SqlCommand(insertQuery, connection))
+                    {
+                        insertCommand.Parameters.AddWithValue("@FactoryId", factoryId);
+                        insertCommand.Parameters.AddWithValue("@NationalId", nationalId);
+                        insertCommand.Parameters.AddWithValue("@Name", name);
+                        insertCommand.Parameters.AddWithValue("@ContactNo", contactNo);
+                        insertCommand.Parameters.AddWithValue("@Email", email);
+                        insertCommand.Parameters.AddWithValue("@Gender", gender);
+                        insertCommand.Parameters.AddWithValue("@Address", address);
+                        insertCommand.Parameters.AddWithValue("@DateOfBirth", dateOfBirth);
+                        insertCommand.Parameters.AddWithValue("@DateOfJoin", dateOfJoin);
+
+                        insertCommand.ExecuteNonQuery();
+                        MessageBox.Show($"Worker registered successfully! Factory ID: {factoryId}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private string GenerateUniqueId(SqlConnection connection)
+        {
+            string maxIdQuery = "SELECT MAX(factory_id) FROM workers WHERE factory_id LIKE 'CT/%'";
+            using (SqlCommand command = new SqlCommand(maxIdQuery, connection))
+            {
+                var result = command.ExecuteScalar()?.ToString();
+                if (string.IsNullOrEmpty(result))
+                {
+                    return "CT/0001";
+                }
+
+                // Extract the numeric part from the last generated ID
+                string lastNumberPart = result.Substring(3); // Remove the 'CT/' part
+                int lastNumber;
+                if (int.TryParse(lastNumberPart, out lastNumber))
+                {
+                    return $"CT/{(lastNumber + 1).ToString("D4")}";
+                }
+                else
+                {
+                    throw new Exception("Invalid format of existing factory_id in the database.");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtNationalId.Clear();
+            txtName.Clear();
+            txtContactNo.Clear();
+            txtEmail.Clear();
+            cmbGender.SelectedIndex = -1;
+            txtAddress.Clear();
+            dtpDateOfBirth.Value = DateTime.Now;
+            dtpDateOfJoin.Value = DateTime.Now;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+        }
+    }
+}
